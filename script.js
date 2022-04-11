6d57a13c52f3a834b6b83ef47ea2cfb5f64a3777
dlnhnr <30333417+dlnhnr@users.noreply.github.com>
2022-04-10 22:58:52 -0400

    eval

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const calculatorInput = document.querySelector("#calculatorInput")
+const input = document.querySelector("#calculatorInput")
 const numbersOperators = document.querySelectorAll(".inputElements")
 
 for(el of numbersOperators) {
   el.addEventListener("click", function() {
-    calculatorInput.append(this.text)
+    input.append(this.innerHTML)
   })
 }
 
 const equals = document.querySelector(".evaluate")
 equals.addEventListener("click", function() {
-  calculatorInput.innerHTML = eval(calculatorInput.text)
+  input.innerHTML = eval(input.innerHTML)
 })
 
 const clear = document.querySelector(".clear")
 clear.addEventListener("click", function() {
-  calculatorInput.innerHTML = ""
+  input.innerHTML = ""
 })
